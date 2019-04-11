17de80c8f568857a1e43bacd2d98d9d096e610e5
andrhage <37300671+andrhage@users.noreply.github.com>
2019-04-11 20:03:48 +0200

    Adding U-net

--- train.py
+++ train.py
@@ -16,6 +16,15 @@ import configparser
 import cv2
 from skimage import measure
 from scipy import ndimage as ndi
+import pdb
+from keras.models import Model, load_model
+from keras.layers import Input
+from keras.layers.core import Dropout, Lambda
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.merge import concatenate
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras import backend as K
 
 
 config = configparser.ConfigParser()
@@ -130,9 +139,10 @@ def fetch_data(debug_mode=config['train/test/debug'].getboolean('debug_mode'), t
                                    int(config['data_processing']['y_pic'])))
             img = img.astype(float) / 255
             seg = seg.astype(float)
+            #pdb.set_trace()
             seg = np.array((seg - np.min(seg)) / (np.max(seg) - np.min(seg)))
             seg = seg[:, :, 0, None]
-            #seg = seg[:, :, 2, None]
+            # seg = seg[:, :, 2, None]
 
             x.append(img)
             y.append(seg)
@@ -182,7 +192,7 @@ def split_data(x_data, y_data):
     return np.array(x_train), np.array(x_val), np.array(y_train), np.array(y_val)
 
 
-def keras_model(input_shape):
+def keras_model_sequential(input_shape):
     """
     The conv net model implemented with Keras
 
@@ -201,11 +211,77 @@ def keras_model(input_shape):
     model.add(layers.Conv2DTranspose(32, 5, strides=(2, 2), padding='same', activation='relu'))
     model.add(layers.Conv2DTranspose(16, 5, strides=(2, 2), padding='same', activation='relu'))
     model.add(layers.Conv2DTranspose(16, 5, strides=(2, 2), padding='same', activation='relu'))
-    # Activation layer
+    # Output layer
     model.add(layers.Conv2D(1, 1, strides=(1, 1), padding='same', activation='sigmoid'))
     # Compile loss and optimizer, and printing the network structure summary
     model.summary()
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+
+    return model
+
+
+def keras_model_residual():
+    """
+
+    :param input_shape:
+    :return:
+    """
+
+    # U-Net model
+    inputs = Input((224, 224, 3))
+    c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(inputs)
+    c1 = Dropout(0.1)(c1)
+    c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
+    p1 = MaxPooling2D((2, 2))(c1)
+
+    c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
+    c2 = Dropout(0.1)(c2)
+    c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
+    p2 = MaxPooling2D((2, 2))(c2)
+
+    c3 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
+    c3 = Dropout(0.2)(c3)
+    c3 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
+    p3 = MaxPooling2D((2, 2))(c3)
+
+    c4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
+    c4 = Dropout(0.2)(c4)
+    c4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
+    p4 = MaxPooling2D(pool_size=(2, 2))(c4)
+
+    c5 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
+    c5 = Dropout(0.3)(c5)
+    c5 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
+
+    u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
+    u6 = concatenate([u6, c4])
+    c6 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
+    c6 = Dropout(0.2)(c6)
+    c6 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
+
+    u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same') (c6)
+    u7 = concatenate([u7, c3])
+    c7 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
+    c7 = Dropout(0.2)(c7)
+    c7 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
+
+    u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
+    u8 = concatenate([u8, c2])
+    c8 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
+    c8 = Dropout(0.1)(c8)
+    c8 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
+
+    u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
+    u9 = concatenate([u9, c1], axis=3)
+    c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
+    c9 = Dropout(0.1)(c9)
+    c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
+
+    outputs = Conv2D(1, (1, 1), activation='sigmoid')(c9)
+
+    model = Model(inputs=[inputs], outputs=[outputs])
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    model.summary()
 
     return model
 
@@ -215,19 +291,39 @@ def setup_model_and_tensorboard(x_train):
     Sets up or loads a model
     :return:
     """
-    model = keras_model(x_train[0].shape)
+    if config['Network'].getboolean('sequential') is True:
+        model = keras_model_sequential(x_train[0].shape)
 
-    tensorboard_callback = callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, batch_size=8, write_graph=True,
-                                                 write_grads=True, write_images=True,
-                                                 embeddings_freq=0, embeddings_layer_names=None,
-                                                 embeddings_metadata=None, embeddings_data=None,
-                                                 update_freq='epoch'
-                                                 )
-    checkpoint = callbacks.ModelCheckpoint('models/weights.{epoch:02d}-{val_acc:.2f}.hdf5', monitor='val_acc',
-                                           verbose=1, save_best_only=config['train/test/debug'].getboolean('save_best'),
-                                           save_weights_only=False, mode='auto', period=1)
+        tensorboard_callback = callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, batch_size=8,
+                                                     write_graph=True,
+                                                     write_grads=True, write_images=True,
+                                                     embeddings_freq=0, embeddings_layer_names=None,
+                                                     embeddings_metadata=None, embeddings_data=None,
+                                                     update_freq='epoch'
+                                                     )
+        checkpoint = callbacks.ModelCheckpoint('models/weights.{epoch:02d}-{val_acc:.2f}.hdf5', monitor='val_acc',
+                                               verbose=1,
+                                               save_best_only=config['train/test/debug'].getboolean('save_best'),
+                                               save_weights_only=False, mode='auto', period=1)
+        return model, [tensorboard_callback, checkpoint]
 
-    return model, [tensorboard_callback, checkpoint]
+    elif config['Network'].getboolean('residual') is True:
+        model_res = keras_model_residual()
+
+        tensorboard_callback = callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, batch_size=10,
+                                                     write_graph=True,
+                                                     write_grads=True, write_images=True,
+                                                     embeddings_freq=0, embeddings_layer_names=None,
+                                                     embeddings_metadata=None, embeddings_data=None,
+                                                     update_freq='epoch'
+                                                     )
+        checkpoint_res = callbacks.ModelCheckpoint('models/weights.{epoch:02d}.hdf5',
+                                                   verbose=1,
+                                               save_best_only=config['train/test/debug'].getboolean('save_best'),
+                                               save_weights_only=False, mode='auto', period=1)
+        #early_stop = EarlyStopping(patience=5, verbose=1)
+
+        return model_res, [tensorboard_callback, checkpoint_res]
 
 
 def main():
@@ -241,15 +337,25 @@ def main():
         # Split data
         x_train, x_val, y_train, y_val = split_data(x_data, y_data)
 
-        # Create model
-        model, callbacks_model = setup_model_and_tensorboard(x_train)
+        if config['Network'].getboolean('sequential') is True:
+            # Create model
+            model, callbacks_model = setup_model_and_tensorboard(x_train)
 
-        # Train model
-        models.Sequential.fit(model, x_train, y_train, batch_size=10,
-                              epochs=int(config['train/test/debug']['epochs']),
-                              verbose=1, validation_data=(x_val, y_val),
-                              shuffle=True, callbacks=callbacks_model
-                              )
+            # Train model
+            models.Sequential.fit(model, x_train, y_train, batch_size=10,
+                                  epochs=int(config['train/test/debug']['epochs']),
+                                  verbose=1, validation_data=(x_val, y_val),
+                                  shuffle=True, callbacks=callbacks_model
+                                  )
+        elif config['Network'].getboolean('residual') is True:
+            # Create model
+            model_res, callbacks_model_res = setup_model_and_tensorboard(x_train)
+
+            # Train model
+            model_res.fit(x_train, y_train, batch_size=10,
+                      epochs=int(config['train/test/debug']['epochs']),
+                      shuffle=True, validation_data=(x_val, y_val),
+                      callbacks=callbacks_model_res)
 
     elif mode == 'testing':
 
@@ -279,11 +385,6 @@ def main():
             # Fetch test data set
             x_test = fetch_data(debug_mode=config['train/test/debug'].getboolean('debug_mode'), test=True)
 
-            # Load best weights from models
-            model = models.load_model('models/' + config['predictions']['weights'])
-            # Plots the layers in terminal summary
-            model.summary()
-
             # Grab all the image names from each folder
             image_names = filenames('data', test=True)
 
@@ -292,16 +393,25 @@ def main():
             for img, names in zip(x_test, image_names):
                 img = np.reshape(img, (1, int(config['data_processing']['x_pic']),
                                        int(config['data_processing']['y_pic']), 3))
-                # Predicts the road
-                prediction = models.Sequential.predict(model, img)
+                if config['Network'].getboolean('sequential') is True:
+                    # Load best weights from models
+                    model = models.load_model('models/' + config['predictions']['weights'])
+                    # Plots the layers in terminal summary
+                    model.summary()
+                    # Predicts the road
+                    prediction = models.Sequential.predict(model, img)
+                elif config['Network'].getboolean('residual') is True:
+                    model = load_model('models/' + config['predictions']['weights'])
+                    # Predicts the road
+                    prediction = model.predict(img)
                 # Filter out values with less certainty than 65 %
                 prediction = np.where(prediction > 0.65, np.ones_like(prediction),
                                       np.zeros_like(prediction))
                 prediction = np.squeeze(prediction)
                 # Processing with morphological operations
                 kernel = np.ones((3, 3), np.uint8)
-                prediction = cv2.erode(prediction, kernel, iterations=1)
-                prediction = cv2.dilate(prediction, kernel, iterations=1)
+                prediction = cv2.erode(prediction, kernel, iterations=4)
+                prediction = cv2.dilate(prediction, kernel, iterations=2)
 
                 # The cca is based on this tutorial:
                 # https://www.pyimagesearch.com/2016/10/31/detecting-multiple-bright-spots-in-an-image-with-python-and-opencv/
@@ -320,8 +430,8 @@ def main():
                 fig, (ax1, ax2) = plt.subplots(1, 2)
                 # ax1.imshow(np.squeeze(prediction))
                 ax2.imshow(np.squeeze(img))
-                # ax1.imshow(prediction)
-                ax1.imshow(mask)
+                ax1.imshow(prediction)
+                # ax1.imshow(mask)
                 fig.savefig(('{}/' + str(names[20:-4]) +
                              '_img_gt.png').format('D:/Masteroppgave/Master-thesis/predictions'))
                 plt.show(block=False)
