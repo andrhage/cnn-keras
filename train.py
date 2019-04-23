6d9705efe641c2ddd39e573a0d14451ed938abf0
andrhage <37300671+andrhage@users.noreply.github.com>
2019-04-24 00:39:46 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-
+from PIL import Image
 import glob
 import os
 import numpy as np
@@ -7,11 +7,9 @@ from scipy import misc
 import sklearn.model_selection
 import sklearn
 import matplotlib.pyplot as plt
-import tensorflow as tf
 from keras import models
 from keras import layers
 from keras import callbacks
-from keras import backend
 from tqdm import tqdm
 import configparser
 import cv2
@@ -25,6 +23,8 @@ from keras.layers.pooling import MaxPooling2D
 from keras.layers.merge import concatenate
 from keras.callbacks import EarlyStopping
 from keras.preprocessing.image import ImageDataGenerator
+import h5py
+from keras import applications
 
 config = configparser.ConfigParser()
 config.read("config.ini")
@@ -81,6 +81,11 @@ def fetch_data(debug_mode=config['train/test/debug'].getboolean('debug_mode'), t
             if config['data_processing'].getboolean('kitti') is True:
                 # Gt with pink/blue road
                 seg = seg[:, :, 2, None]
+            elif config['data_processing'].getboolean('freiburg') is True:
+                bg = seg[:, :, 0] == seg[:, :, 1]  # B == G
+                gr = seg[:, :, 1] == seg[:, :, 2]  # G == R
+                seg = np.bitwise_and(bg, gr, dtype=np.uint8) * 255
+                seg = np.repeat(seg[..., None], 1, axis=2)
             else:
                 # Gt with red road
                 seg = seg[:, :, 0, None]
@@ -169,7 +174,7 @@ def keras_model_residual():
     """
 
     # U-Net model
-    inputs = Input((224, 224, 3))
+    inputs = Input((int(config['data_processing']['x_pic']), int(config['data_processing']['y_pic']), 3))
     c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(inputs)
     c1 = Dropout(0.1)(c1)
     c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
@@ -246,6 +251,7 @@ def setup_model_and_tensorboard(x_train):
                                                verbose=1,
                                                save_best_only=config['train/test/debug'].getboolean('save_best'),
                                                save_weights_only=False, mode='auto', period=1)
+
         return model, [tensorboard_callback, checkpoint]
 
     elif config['Network'].getboolean('residual') is True:
@@ -262,9 +268,98 @@ def setup_model_and_tensorboard(x_train):
                                                    verbose=1,
                                                save_best_only=config['train/test/debug'].getboolean('save_best'),
                                                save_weights_only=False, mode='auto', period=1)
-        #early_stop = EarlyStopping(patience=5, verbose=1)
+
+        # early_stop = EarlyStopping(patience=5, verbose=1)
 
         return model_res, [tensorboard_callback, checkpoint_res]
+
+
+def cca_algorithm(prediction):
+    """
+    # The cca is based on this tutorial:
+    # https://www.pyimagesearch.com/2016/10/31/detecting-multiple-bright-spots-in-an-image-with-python-and-opencv/
+    :param prediction:
+    :return:
+    """
+    cca = measure.label(prediction, connectivity=None, background=0)
+    mask = np.zeros(prediction.shape, dtype="uint8")
+    for labels in np.unique(cca):
+        if labels == 0:
+            continue
+        labelmask = np.zeros(prediction.shape, dtype="uint8")
+        labelmask[cca == labels] = 255
+        count_pixels = cv2.countNonZero(labelmask)
+        if count_pixels > int(config['predictions']['threshold']):
+            mask = cv2.add(mask, labelmask)
+            mask = ndi.binary_fill_holes(mask)
+    return mask
+
+
+def predict(x_test, image_names, model):
+    """
+
+    :param x_test:
+    :param image_names:
+    :param model:
+    :return:
+    """
+    i = 0
+    for img, names in zip(x_test, image_names):
+
+        img = np.reshape(img, (1, int(config['data_processing']['x_pic']),
+                               int(config['data_processing']['y_pic']), 3))
+
+        # Predicts the road
+        prediction = model.predict(img)
+        # Filter out values with less certainty than 65 %
+        prediction = np.where(prediction > 0.65, np.ones_like(prediction),
+                              np.zeros_like(prediction))
+        prediction = np.squeeze(prediction)
+        # Processing with morphological operations
+        kernel = np.ones((3, 3), np.uint8)
+        prediction = cv2.erode(prediction, kernel, iterations=3)
+        prediction = cv2.dilate(prediction, kernel, iterations=2)
+        # Processing with connected component analysis
+        mask = cca_algorithm(prediction)
+        fig, (ax1, ax2) = plt.subplots(1, 2)
+        # ax1.imshow(np.squeeze(prediction))
+        ax2.imshow(np.squeeze(img))
+
+        # With or without cca
+        if config['predictions'].getboolean('cca') is True:
+            ax1.imshow(mask)
+            fig.savefig(('{}/' + str(names[20:-4]) +
+                         '.png').format('D:/Masteroppgave/Master-thesis/predictions'))
+            if config['predictions'].getboolean('plots') is True:
+                plt.show(block=False)
+                plt.pause(0.5)
+            i += 1
+            plt.close()
+
+            mask = np.reshape(mask, (int(config['data_processing']['x_pic']),
+                                     int(config['data_processing']['y_pic']), 1)) * 255.0
+            dump = np.zeros([np.shape(mask)[0], np.shape(mask)[1], 2])
+            mask = np.concatenate((mask, dump), 2)
+            misc.imsave(('{}/' + str(names[20:-4]) +
+                         '.png').format('D:/Masteroppgave/Master-thesis/predictions/gt'), mask)
+        else:
+            ax1.imshow(prediction)
+
+            fig.savefig(('{}/' + str(names[20:-4]) +
+                         '.png').format('D:/Masteroppgave/Master-thesis/predictions'))
+            if config['predictions'].getboolean('plots') is True:
+                plt.show(block=False)
+                plt.pause(0.5)
+            i += 1
+            plt.close()
+
+            prediction = np.reshape(prediction, (int(config['data_processing']['x_pic']),
+                                                 int(config['data_processing']['y_pic']), 1)) * 255.0
+            dump = np.zeros([np.shape(prediction)[0], np.shape(prediction)[1], 2])
+            prediction = np.concatenate((prediction, dump), 2)
+            misc.imsave(('{}/' + str(names[20:-4]) +
+                         '.png').format('D:/Masteroppgave/Master-thesis/predictions/gt'),
+                        prediction)
 
 
 def main():
@@ -314,6 +409,13 @@ def main():
                 # Create model
                 model_res, callbacks_model_res = setup_model_and_tensorboard(x_train)
 
+                # Transfer learning
+                # model_res = applications.vgg16.VGG16(include_top=True, weights='imagenet', input_tensor=None,
+                #                                  input_shape=(224, 224, 3), pooling=None, classes=1000)
+                # for layer in model_res.layers:
+                #     layer.trainable = False
+                # model_res.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
                 # Train model
                 model_res.fit(x_train, y_train, batch_size=10,
                               epochs=int(config['train/test/debug']['epochs']),
@@ -351,7 +453,6 @@ def main():
             # Grab all the image names from each folder
             image_names = filenames('data', test=True)
 
-            i = 0
             # Connect each image with its name and run through them one by one
             if config['Network'].getboolean('sequential') is True:
                 # Load best weights from models
@@ -360,85 +461,8 @@ def main():
             elif config['Network'].getboolean('residual') is True:
                 model = load_model('models/' + config['predictions']['weights'])
 
-            for img, names in zip(x_test, image_names):
-
-                img = np.reshape(img, (1, int(config['data_processing']['x_pic']),
-                                       int(config['data_processing']['y_pic']), 3))
-
-                # if config['Network'].getboolean('sequential') is True:
-                #     # Load best weights from models
-                #     #model = models.load_model('models/' + config['predictions']['weights'])
-                #     # Predicts the road
-                #     prediction = models.Sequential.predict(model, img)
-                # elif config['Network'].getboolean('residual') is True:
-                #     #model = load_model('models/' + config['predictions']['weights'])
-
-                #     # Predicts the road
-                prediction = model.predict(img)
-                # Filter out values with less certainty than 65 %
-                prediction = np.where(prediction > 0.65, np.ones_like(prediction),
-                                      np.zeros_like(prediction))
-                prediction = np.squeeze(prediction)
-                # Processing with morphological operations
-                kernel = np.ones((3, 3), np.uint8)
-                prediction = cv2.erode(prediction, kernel, iterations=3)
-                prediction = cv2.dilate(prediction, kernel, iterations=2)
-
-                # The cca is based on this tutorial:
-                # https://www.pyimagesearch.com/2016/10/31/detecting-multiple-bright-spots-in-an-image-with-python-and-opencv/
-                cca = measure.label(prediction, connectivity=None, background=0)
-                mask = np.zeros(prediction.shape, dtype="uint8")
-                for labels in np.unique(cca):
-                    if labels == 0:
-                        continue
-                    labelmask = np.zeros(prediction.shape, dtype="uint8")
-                    labelmask[cca == labels] = 255
-                    count_pixels = cv2.countNonZero(labelmask)
-                    if count_pixels > int(config['predictions']['threshold']):
-                        mask = cv2.add(mask, labelmask)
-                        mask = ndi.binary_fill_holes(mask)
-                # ---------------------------------------------------------------------------------------------------------------
-                fig, (ax1, ax2) = plt.subplots(1, 2)
-                # ax1.imshow(np.squeeze(prediction))
-                ax2.imshow(np.squeeze(img))
-
-                # With or without cca
-                if config['predictions'].getboolean('cca') is True:
-                    ax1.imshow(mask)
-                    fig.savefig(('{}/' + str(names[20:-4]) +
-                                 '.png').format('D:/Masteroppgave/Master-thesis/predictions'))
-                    if config['predictions'].getboolean('plots') is True:
-                        plt.show(block=False)
-                        plt.pause(0.5)
-                    i += 1
-                    plt.close()
-
-                    mask = np.reshape(mask, (int(config['data_processing']['x_pic']),
-                                                             int(config['data_processing']['y_pic']), 1)) * 255.0
-                    dump = np.zeros([np.shape(mask)[0], np.shape(mask)[1], 2])
-                    mask = np.concatenate((mask, dump), 2)
-                    misc.imsave(('{}/' + str(names[20:-4]) +
-                                 '.png').format('D:/Masteroppgave/Master-thesis/predictions/gt'),
-                                mask)
-                else:
-                    ax1.imshow(prediction)
-
-                    fig.savefig(('{}/' + str(names[20:-4]) +
-                                 '.png').format('D:/Masteroppgave/Master-thesis/predictions'))
-                    if config['predictions'].getboolean('plots') is True:
-                        plt.show(block=False)
-                        plt.pause(0.5)
-                    i += 1
-                    plt.close()
-
-                    prediction = np.reshape(prediction, (int(config['data_processing']['x_pic']),
-                                                         int(config['data_processing']['y_pic']), 1)) * 255.0
-                    dump = np.zeros([np.shape(prediction)[0], np.shape(prediction)[1], 2])
-                    prediction = np.concatenate((prediction, dump), 2)
-                    misc.imsave(('{}/' + str(names[20:-4]) +
-                                 '.png').format('D:/Masteroppgave/Master-thesis/predictions/gt'),
-                                prediction)
-                #backend.clear_session()
+            # Predicting the output
+            predict(x_test, image_names, model)
 
 
 if __name__ == '__main__':
