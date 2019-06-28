adb1140d86e88267676dd810de301031accfe79c
andrhage <37300671+andrhage@users.noreply.github.com>
2019-06-28 11:52:47 +0200

    Updated scripts

--- convert_files.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-import glob
-
-
-for filename in glob.iglob('/Masteroppgave/Master-thesis/data/testing/images/**/*.jpg', recursive=True):
-    print(filename[0:-4])
-    im = Image.open(filename)
-    im.save(filename[0:-4] + '.png')
